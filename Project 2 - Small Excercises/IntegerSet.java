480841c457c34900e3d5778052af660f9e4d42e9
Sebastian Metellus <65518883+smetellus01@users.noreply.github.com>
2022-07-18 00:36:58 -0400

    Update IntegerSet.java

--- Project 2 - Small Excercises/IntegerSet.java
+++ Project 2 - Small Excercises/IntegerSet.java
@@ -1,1 +1,93 @@
+public class IntegerSet 
+{
+	private boolean [] SetInt;
+	
+	public IntegerSet()
+	{
+		SetInt = new boolean [101];
+		for(int i = 0; i <= 100; i++)
+		{
+			SetInt[i] = false;
+		}
+	};
 
+    public IntegerSet insertElement(int data)
+	{
+		this.SetInt[data] = true;
+		return this;
+	};
+	
+	public IntegerSet deleteElement(int data)
+	{
+		this.SetInt[data] = false;
+		return this;
+	};
+
+    public IntegerSet union(IntegerSet iSet)
+    {
+        IntegerSet TempSet = new IntegerSet();
+        
+        for(int i = 0; i <= 100; i++)
+        {
+            if(this.SetInt[i] == true || this.SetInt[i] == true)
+            {
+                TempSet.SetInt[i] = true;
+            }
+        }
+
+        return TempSet; 
+    };
+
+    public IntegerSet intersection(IntegerSet iSet)
+	{
+        IntegerSet TempSet = new IntegerSet();
+
+        for(int i = 0; i <= 100; i++)
+        {
+            if(this.SetInt[i] == true && iSet.SetInt[i] == true)
+            {
+                    TempSet.SetInt[i] = true;
+            }
+        }
+
+        return TempSet;
+    };
+    
+    public boolean isEqualTo(IntegerSet iSet)
+	{
+        boolean result = true;
+
+        for(int i = 0; i <= 100; i++)
+        {
+            if(this.SetInt[i] != iSet.SetInt[i])
+            {
+                result = false;
+            }
+        }
+
+        return result;
+    };
+
+    public String toString()
+	{
+        boolean Empty = true;
+        String test = new String();
+
+        for(int i = 0; i <= 100; i++)
+        {
+            if(this.SetInt[i] == true)
+            {
+                test = new String(test + " " + i);
+                Empty = false;
+            }
+        }
+
+        if(Empty = true)
+        {
+            test = new String("---");
+        }
+
+        return test;
+    };
+
+}
